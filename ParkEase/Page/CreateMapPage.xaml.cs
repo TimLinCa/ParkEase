63b177376a7e6038db988a8e6428a72507032c3a
Sang Lee <tkdgus2233440@gmail.com>
2024-05-18 18:19:28 -0600

    Update CreateMapPage.xaml, CreateMapPage.xaml.cs, CreateMapViewModel.cs

--- ParkEase/Page/CreateMapPage.xaml.cs
+++ ParkEase/Page/CreateMapPage.xaml.cs
@@ -2,8 +2,35 @@ namespace ParkEase.Page;
 
 public partial class CreateMapPage : ContentPage
 {
-	public CreateMapPage()
-	{
-		InitializeComponent();
-	}
+    private double initialWidth;
+    private double initialHeight;
+    private double initialX;
+    private double initialY;
+
+    public CreateMapPage()
+    {
+        InitializeComponent();
+    }
+    //Sang
+    private async void UploadImageClicked(object sender, EventArgs e)
+    {
+        if (MediaPicker.Default.IsCaptureSupported)
+        {
+            // Load photo
+            FileResult myPhoto = await MediaPicker.Default.PickPhotoAsync();
+            if (myPhoto != null)
+            {
+                // Display the image
+                UploadedImage.Source = ImageSource.FromFile(myPhoto.FullPath);
+            }
+        }
+        else
+        {
+            await Shell.Current.DisplayAlert("OOPS", "Your device isn't supported", "OK");
+        }
+    }
+    //Sang
+
 }
+
+
--- ParkEase/ViewModel/CreateMapViewModel.cs
+++ ParkEase/ViewModel/CreateMapViewModel.cs
@@ -10,5 +10,28 @@ namespace ParkEase.ViewModel
     public partial class CreateMapViewModel : ObservableObject
     {
 
+        /*private async void OnCounterClicked(object sender, EventArgs e)
+        {
+            if (MediaPicker.Default.IsCaptureSupported)
+            {
+                //Load photo
+                FileResult myPhoto = await MediaPicker.Default.PickPhotoAsync();
+                if (myPhoto != null)
+                {
+                    //save the image captured in the application.
+                    string localFilePath = Path.Combine(FileSystem.CacheDirectory, myPhoto.FileName);
+                    using Stream sourceStream = await myPhoto.OpenReadAsync();
+                    using FileStream localFileStream = File.OpenWrite(localFilePath);
+                    await sourceStream.CopyToAsync(localFileStream);
+                }
+            }
+            else
+            {
+                await Shell.Current.DisplayAlert("OOPS", "Your device isn't supported", "OK");
+            }
+
+        }*/
+
     }
+    
 }
