4eaac32a3874eddf07468e4f24adeac5dfa295a9
thuymy.lam <thuymylam96@gmail.com>
2024-05-24 13:19:47 -0600

    Add Rectangle class

---
+++ ParkEase.Core/Data/Rectangle.cs
@@ -0,0 +1,21 @@
+﻿using MongoDB.Bson.Serialization.Attributes;
+using MongoDB.Bson;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ParkEase.Core.Data
+{
+    public class Rectangle
+    {
+        [BsonId]
+        [BsonRepresentation(BsonType.ObjectId)]
+        public int id;
+
+        public string Index {  get; set; }
+
+        public RectF Rect {  get; set; }
+    }
+}
