225ea2ba363f0b7b34bff8c16af12d8744186442
cuiyiye <130084009+cuiyiye@users.noreply.github.com>
2024-06-18 15:44:26 -0600

    Update selectRange

--- ParkEase/ViewModel/UserMapViewModel.cs
+++ ParkEase/ViewModel/UserMapViewModel.cs
@@ -1,6 +1,7 @@
 ﻿using CommunityToolkit.Mvvm.ComponentModel;
 using CommunityToolkit.Mvvm.Input;
 using Microsoft.Maui.Controls.Maps;
+using Microsoft.Maui.Maps;
 using Microsoft.Maui.Platform;
 using MongoDB.Bson.Serialization;
 using MongoDB.Driver;
@@ -13,6 +14,7 @@ using ParkEase.Core.Services;
 using System;
 using System.Collections.ObjectModel;
 using System.ComponentModel;
+using System.Diagnostics;
 using System.Runtime.CompilerServices;
 using System.Windows.Input;
 
@@ -20,6 +22,8 @@ namespace ParkEase.ViewModel
 {
     public partial class UserMapViewModel : ObservableObject
     {
+        private List<MapLine> dbMapLines;
+
         [ObservableProperty]
         private ObservableCollection<MapLine> mapLines;
 
@@ -28,6 +32,12 @@ namespace ParkEase.ViewModel
 
         [ObservableProperty]
         private string availableSpots;
+
+        [ObservableProperty]
+        private string selectedRadius;
+
+        [ObservableProperty]
+        private double radius;
 
         private readonly IMongoDBService mongoDBService;
         private readonly IDialogService dialogService;
@@ -63,7 +73,8 @@ namespace ParkEase.ViewModel
                     }
                 }
 
-                MapLines = lines;
+                dbMapLines = new List<MapLine>(lines);
+                //MapLines = lines;
                 await LoadAvailableSpotsAsync(null);
             }
             catch (Exception ex)
@@ -126,5 +137,48 @@ namespace ParkEase.ViewModel
                 System.Diagnostics.Debug.WriteLine($"Error retrieving parking data: {ex.Message}");
             }
         }
+
+        public ICommand UpdateRangeCommand => new RelayCommand(async() =>
+        {
+            // parse selectedRadius to int 200 meters > 0.2
+            if (string.IsNullOrEmpty(SelectedRadius))
+            {
+                Debug.WriteLine("SelectedRadius is null or empty");
+                return;
+            }
+
+            // Parse selected radius to double (meters to kilometers)
+            if (!double.TryParse(SelectedRadius.Split(' ')[0], out double radius_out))
+            {
+                Debug.WriteLine("Failed to parse SelectedRadius");
+                return;
+            }
+
+            radius_out /= 1000.0;
+            var location = await Geolocation.GetLocationAsync();
+
+            List<MapLine> linesInRange = dbMapLines.Where(line => isPointInCircle(line.Points, location.Latitude, location.Longitude, radius_out)).ToList();
+            MapLines = new ObservableCollection<MapLine>(linesInRange);
+            Radius = radius_out;
+        });
+
+
+        private bool isPointInCircle(List<MapPoint> points, double centerLat, double centerLng, double radius)
+        {
+            foreach(MapPoint point in points)
+            {
+                double pointLat = double.Parse(point.Lat);
+                double pointLng = double.Parse(point.Lng);
+
+                var distance = Math.Sqrt(Math.Pow(pointLat - centerLat, 2) + Math.Pow(pointLng - centerLng, 2));
+                if(distance <= (radius / 111))
+                {
+                    return true;
+                }
+            }
+
+            return false;
+        }
+
     }
 }
