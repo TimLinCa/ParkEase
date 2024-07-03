096f5982fd4c01e4038da92c3aca06e863cf7a18
thuymy.lam <thuymylam96@gmail.com>
2024-07-03 11:46:04 -0600

    Adding real-time update PrivateMap

--- ParkEase/ViewModel/PrivateMapViewModel.cs
+++ ParkEase/ViewModel/PrivateMapViewModel.cs
@@ -25,7 +25,7 @@ using System.Runtime.CompilerServices;
 using ParkEase.Page;
 
 namespace ParkEase.ViewModel
-{ 
+{
     public partial class PrivateMapViewModel : ObservableObject
     {
         [ObservableProperty]
@@ -78,15 +78,20 @@ namespace ParkEase.ViewModel
         private string scannerText;
 
         [ObservableProperty]
-        private string scannerImage;
+        private string scannerImage = "scanner_image.png";
 
         [ObservableProperty]
-        private string arrowBack;
+        private string arrowBack = "arrow_icon.png";
 
         private string privateParkingId;
 
         [ObservableProperty]
         private BarcodeDetectionEventArgs barcodeDetectionEventArgs;
+
+        private CancellationTokenSource cts;
+        private readonly object lockObj = new object();
+
+        readonly bool stopping = false;
 
 
         public PrivateMapViewModel(IMongoDBService mongoDBService, IDialogService dialogService, ParkEaseModel model)
@@ -99,23 +104,29 @@ namespace ParkEase.ViewModel
             FloorNames = new ObservableCollection<string>();
             ListRectangleFill = new ObservableCollection<Rectangle>();
             privateStatusData = new List<PrivateStatus>();
-           
+
             BarcodeResult = string.Empty;
             EnableScanner = true;
             GridVisible = false;
             ScannerText = "";
-            scannerImage = "scanner_image.png";
-            arrowBack = "arrow_icon.png";
+            //scannerImage = "scanner_image.png";
+            //arrowBack = "arrow_icon.png";
         }
 
         public ICommand LoadedCommand => new RelayCommand(async () =>
         {
             privateParkingId = parkEaseModel.PrivateMapId;
             await LoadParkingData();
+
+            cts = new CancellationTokenSource();
+            var token = cts.Token;
+            _ = Run(token); // Start the real-time update loop
         });
 
         public ICommand UnLoadedCommand => new RelayCommand(() =>
         {
+            cts?.Cancel(); // Cancel the real-time update loop
+
             parkingLotData?.Clear();
             FloorNames?.Clear();
             listFloorInfos?.Clear();
@@ -166,7 +177,8 @@ namespace ParkEase.ViewModel
 
                 // Filter privateStatusData based on privateParkingId
                 privateStatusData = status.Where(item => item.AreaId == privateParkingId).ToList();
-                if(FloorNames.FirstOrDefault() != null)
+
+                if (FloorNames.FirstOrDefault() != null)
                 {
                     SelectedFloorName = FloorNames.First();
                 }
@@ -177,71 +189,112 @@ namespace ParkEase.ViewModel
             }
         }
 
-        partial void OnSelectedFloorNameChanged(string? value)
+        /*partial void OnSelectedFloorNameChanged(string? value)
         {
             _ = ShowSelectedMap();
-        }
+        }*/
 
-
-        [RelayCommand]
-        public async Task GoSeachPage()
+        private async Task Run(CancellationToken token)
         {
-            try
+            await Task.Run(async () =>
             {
-                await Shell.Current.GoToAsync(nameof(PrivateSearchPage));
-            }
-            catch (Exception ex)
-            {
-                await dialogService.ShowAlertAsync("Error", ex.Message, "OK");
-            }
+                while (!stopping)
+                {
+                    token.ThrowIfCancellationRequested();
+                    try
+                    {
+                        await ShowSelectedMap();
+                        await Task.Delay(2000, token);
+                    }
+                    catch (Exception ex)
+                    {
+                        Console.WriteLine(ex);
+                    }
+                }
+            }, token);
         }
+
+
+        /*        [RelayCommand]
+                public async Task GoSeachPage()
+                {
+                    try
+                    {
+                        await Shell.Current.GoToAsync(nameof(PrivateSearchPage));
+                    }
+                    catch (Exception ex)
+                    {
+                        await dialogService.ShowAlertAsync("Error", ex.Message, "OK");
+                    }
+                }*/
 
         private async Task ShowSelectedMap()
         {
-            ImgSourceData = null;
-            ListRectangleFill.Clear();
-
-            // ShowSelectedMap Function
-            FloorInfo selectedMap = listFloorInfos.FirstOrDefault(data => data.Floor == SelectedFloorName);
-            if (selectedMap == null)
+            try
             {
-                System.Diagnostics.Debug.WriteLine("No selected floor map found.");
-                return;
-            }
+                ImgSourceData = null;
+                ListRectangleFill.Clear();
 
-            // Filter by selectedFloorName and Create a dictionary for quick lookup of statuses by index
-            var matchingStatus = privateStatusData
-                .Where(item => item.Floor == SelectedFloorName)
-                .ToDictionary(item => item.Index, item => item.Status);
-
-            // Fetch image data
-            var imageData = selectedMap.ImageData;
-            using (MemoryStream ms = new MemoryStream(imageData))
-            {
-                ImgSourceData = await Task.Run(() => PlatformImage.FromStream(ms));
-            }
-
-            // Variable to count availability status (false means available lot)
-            int availabilityCount = 0;
-
-            // Update rectangle colors based on status and add them to ListRectangle
-            foreach (var rectangle in selectedMap.Rectangles)
-            {
-                if (matchingStatus.TryGetValue(rectangle.Index, out bool isAvailable))
+                // Fetch PrivateStatus data from MongoDB
+                var status = await mongoDBService.GetData<PrivateStatus>(CollectionName.PrivateStatus);
+                if (status == null || status.Count == 0)
                 {
-                    if (!isAvailable)
-                    {
-                        rectangle.Color = "green";
-                        availabilityCount++;
-                    }
-                    else
-                    {
-                        rectangle.Color = "red";
-                    }
+                    System.Diagnostics.Debug.WriteLine("No private status data found.");
+                    return;
                 }
-                ListRectangleFill.Add(rectangle);
+
+                // Filter privateStatusData based on privateParkingId
+                privateStatusData = status.Where(item => item.AreaId == privateParkingId).ToList();
+
+                // ShowSelectedMap Function
+                FloorInfo selectedMap = listFloorInfos.FirstOrDefault(data => data.Floor == SelectedFloorName);
+                if (selectedMap == null)
+                {
+                    System.Diagnostics.Debug.WriteLine("No selected floor map found.");
+                    return;
+                }
+
+                // Filter by selectedFloorName and Create a dictionary for quick lookup of statuses by index
+                var matchingStatus = privateStatusData
+                    .Where(item => item.Floor == SelectedFloorName)
+                    .ToDictionary(item => item.Index, item => item.Status);
+
+                // Fetch image data
+                var imageData = selectedMap.ImageData;
+                using (MemoryStream ms = new MemoryStream(imageData))
+                {
+                    ImgSourceData = await Task.Run(() => PlatformImage.FromStream(ms));
+                }
+
+                // Variable to count availability status (false means available lot)
+                int availabilityCount = 0;
+                ObservableCollection<Rectangle> rectangles = new ObservableCollection<Rectangle>();
+                // Update rectangle colors based on status and add them to ListRectangle
+                foreach (var rectangle in selectedMap.Rectangles)
+                {
+                    if (matchingStatus.TryGetValue(rectangle.Index, out bool isAvailable))
+                    {
+                        if (!isAvailable)
+                        {
+                            rectangle.Color = "green";
+                            availabilityCount++;
+                        }
+                        else
+                        {
+                            rectangle.Color = "red";
+                        }
+                    }
+                    rectangles.Add(rectangle);
+                }
+                ListRectangleFill = rectangles;
+                //await dialogService.ShowBottomSheet($"{address} {city}", $"{fee} per hour", $"{limitHour}", $"{SelectedFloorName}: {availabilityCount} available lots", false, "", "");
             }
-            await dialogService.ShowBottomSheet($"{address} {city}", $"{fee} per hour", $"{limitHour}", $"{SelectedFloorName}: {availabilityCount} available lots", false, "", "");
+            catch (Exception)
+            {
+
+                throw;
+            }
+         
         }
 
         public ICommand NavigatePrivateSearchPage => new RelayCommand(async () =>
