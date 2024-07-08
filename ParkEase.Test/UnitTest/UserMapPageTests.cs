d610c123f31170f4bebe7a8de432a3d7d1feb463
cuiyiye <130084009+cuiyiye@users.noreply.github.com>
2024-07-08 14:44:06 -0600

    Add UserMapPageTest

---
+++ ParkEase.Test/UnitTest/UserMapPageTests.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+
+
+
+namespace ParkEase.Test.UnitTest
+{
+    public class UserMapPageTests
+    {
+        [Fact]
+        public void LocationDisplayTest()
+        {
+ 
+        }
+    }
+}
